9eb135e430dda451f4addd604865f569f82e2c17
ManuRibeiro <e.ribeiro@ua.pt>
2023-11-03 00:45:09 +0000

    images

---
+++ HealthyFOrest/src/components/header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <img src='../assets/ba665660-7969-11ee-be37-e35334191813Captura de ecrã 2023-11-01 191653.svg'></img>
+      <a href="#" className="nav-link">Project</a>
+      <a href="#" className="nav-link">About</a>
+    </div>
+  );
+}
+
+export default Navbar;
